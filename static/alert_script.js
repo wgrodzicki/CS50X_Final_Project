c0299185fc6c9dacba2967af6d2c27d170ec0216
Wojciech Grodzicki <wojciech_grodzicki@outlook.com>
2023-05-30 17:29:36 +0200

    CS50x_Final_Project

---
+++ static/alert_script.js
@@ -0,0 +1,8 @@
+// Script handling the alert
+
+// Wait until the page loads
+window.addEventListener("load", function() {
+
+    alert("There is no mobile support as for now. For the full experience, please launch the game on a keyboard-equipped device, such as laptop or PC.");
+
+});
